9b93cb5edd79bddcb21c2bce56368787490dc674
Stefanos Kypritidis <44138542+stef4k@users.noreply.github.com>
2021-03-02 02:11:29 +0200

    Delete src/vrp directory

--- src/vrp/TwoOptMove.java
+++
@@ -1,9 +0,0 @@
-package vrp;
-
-public class TwoOptMove {
-    int positionOfFirstRoute;
-    int positionOfSecondRoute;
-    int positionOfFirstNode;
-    int positionOfSecondNode;
-    double moveAddedDistance;
-}
